a76f7c0ab3228d7f84019488c5af48e6bafce0a9
AndreyVoenkov <voenkov.java.dev@gmail.co>
2024-12-14 17:37:35 +0300

    14.12.2024

--- src/main/java/com/mysensei/mysensei/Controller/IndexController.java
+++ src/main/java/com/mysensei/mysensei/Controller/IndexController.java
@@ -1,13 +1,13 @@
-//package com.mysensei.mysensei.Controller;
-//
-//import org.springframework.stereotype.Controller;
-//import org.springframework.web.bind.annotation.GetMapping;
-//
-//@Controller
-//public class IndexController {
-//
-//    @GetMapping("/index")
-//    public String index() {
-//        return "index"; // Возвращает файл index.html
-//    }
-//}
+package com.mysensei.mysensei.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/index")
+    public String index() {
+        return "index"; // Возвращает файл index.html
+    }
+}
